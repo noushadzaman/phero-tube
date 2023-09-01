e2920d40e41d859f70eb8ec9bc76fa25c4717371
Noushadozi <noushadozi333@gmail.com>
2023-09-01 19:18:01 +0600

    no content section added

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const allCategories = async () => {
-  const btnContainer = document.getElementById("btn-container");
   fetch("https://openapi.programming-hero.com/api/videos/categories")
     .then((res) => res.json(res))
     .then((data) => handleBtn(data.data));
@@ -10,56 +9,71 @@ const handleBtn = (data) => {
   const btnContainer = document.getElementById("btn-container");
   data.forEach((category) => {
     const div = document.createElement("div");
-    div.innerHTML = `<button class="btn btn-active">${category.category}</button>`;
+    div.innerHTML = `<button onclick="loadData(${category.category_id})" class="btn btn-active">${category.category}</button>`;
     btnContainer.appendChild(div);
   });
 };
 
-const loadData = () => {
-  fetch("https://openapi.programming-hero.com/api/videos/category/1000")
+const loadData = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     .then((res) => res.json())
     .then((data) => setCard(data.data));
-};
-
-const setCard = (data) => {
-  console.log(data);
-  const cardContainer = document.getElementById("card-container");
-  data.forEach((singleData) => {
-    const sec = singleData.others.posted_date;
-    const min = sec / 60;
-    const hour = parseInt(min / 60);
-    const time = (min / 60).toFixed(2);
-    const card = document.createElement("div");
-    card.innerHTML = `
+  };
+  
+  const setCard = (data) => {
+    console.log(data);
+    if (data.length == 0) {
+      const cardContainer = document.getElementById("card-container");
+      cardContainer.classList.remove("grid");
+      cardContainer.innerHTML = "";
+      cardContainer.innerHTML = `
+      <div class="h-[70vh] text-center flex flex-col items-center justify-center">
+      <img src="./Icon.png" />
+      <h1 class="text-[32px]">Oops!! Sorry, There is no content here</h1>  
+      </div>
+      `;
+  } else {
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.classList.add("grid");
+    cardContainer.innerHTML = "";
+    data.forEach((singleData) => {
+      const sec = singleData.others.posted_date;
+      const min = sec / 60;
+      const hour = parseInt(min / 60);
+      const time = (min / 60).toFixed(2);
+      const card = document.createElement("div");
+      card.innerHTML = `
     <div class="card w-96 bg-base-100 shadow-xl">
     <figure class="relative"><img class="w-[100%] h-[216px]" src=${
       singleData.thumbnail
     } alt="Shoes" />
-    <p class="bg-[#171717] text-[#fff] bottom-[12px] right-[12px] rounded absolute">${hour? hour+'hrs '+time.slice(2,4)+'min' : []}</p>
+    <p class="px-[6px] bg-[#171717] text-[#fff] bottom-[12px] right-[12px] rounded absolute">${
+      hour ? hour + "hrs " + time.slice(2, 4) + "min" : []
+    }</p>
     </figure>
     <div class="card-body">
-      <div class="flex gap-[12px]">
-      <img class="w-[40px] h-[40px] rounded-3xl" src=${
-        singleData.authors[0].profile_picture
-      }/>
-      <h2 class="card-title">
-        ${singleData.title}<span>${
-      singleData.authors[0].verified
-        ? '<img src="./fi_10629607.svg">'
-        : "<img/>"
-    } </span>
-      </h2>
-      </div> 
-      <p class="text-[#5d5d5d] text-[14px]">${
-        singleData.authors[0].profile_name
-      }</p>
-      <p>${singleData.others.posted_date}</p>
-      <p class="text-[#5d5d5d] text-[14px]">${singleData.others.views}</p>
+    <div class="flex gap-[12px]">
+    <img class="w-[40px] h-[40px] rounded-3xl" src=${
+      singleData.authors[0].profile_picture
+    }/>
+    <h2 class="card-title">
+    ${singleData.title}<span>${
+        singleData.authors[0].verified
+          ? '<img src="./fi_10629607.svg">'
+          : "<img/>"
+      } </span>
+    </h2>
+    </div> 
+    <p class="text-[#5d5d5d] text-[14px]">${
+      singleData.authors[0].profile_name
+    }</p>
+      <p class="text-[#5d5d5d] text-[14px]">${singleData.others.views} views</p>
     </div>
-  </div>
+    </div>
     `;
-    cardContainer.appendChild(card);
-  });
+      cardContainer.appendChild(card);
+    });
+  }
 };
 
-loadData();
+loadData(1000);
