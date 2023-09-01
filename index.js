1aef6e6c5fc4a0a4a189db33946a40fb763bce16
Noushadozi <noushadozi333@gmail.com>
2023-09-01 16:09:03 +0600

    time added

--- index.js
+++ index.js
@@ -25,12 +25,18 @@ const setCard = (data) => {
   console.log(data);
   const cardContainer = document.getElementById("card-container");
   data.forEach((singleData) => {
+    const sec = singleData.others.posted_date;
+    const min = sec / 60;
+    const hour = parseInt(min / 60);
+    const time = (min / 60).toFixed(2);
     const card = document.createElement("div");
     card.innerHTML = `
     <div class="card w-96 bg-base-100 shadow-xl">
-    <figure><img class="w-[100%] h-[216px]" src=${
+    <figure class="relative"><img class="w-[100%] h-[216px]" src=${
       singleData.thumbnail
-    } alt="Shoes" /></figure>
+    } alt="Shoes" />
+    <p class="bg-[#171717] text-[#fff] bottom-[12px] right-[12px] rounded absolute">${hour? hour+'hrs '+time.slice(2,4)+'min' : []}</p>
+    </figure>
     <div class="card-body">
       <div class="flex gap-[12px]">
       <img class="w-[40px] h-[40px] rounded-3xl" src=${
@@ -47,6 +53,7 @@ const setCard = (data) => {
       <p class="text-[#5d5d5d] text-[14px]">${
         singleData.authors[0].profile_name
       }</p>
+      <p>${singleData.others.posted_date}</p>
       <p class="text-[#5d5d5d] text-[14px]">${singleData.others.views}</p>
     </div>
   </div>
