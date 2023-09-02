c129604681c4084147f52d467406cf6895990aea
Noushadozi <noushadozi333@gmail.com>
2023-09-02 06:17:01 +0600

    sorting by view function added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let array = [];
+
 const allCategories = async () => {
   fetch("https://openapi.programming-hero.com/api/videos/categories")
     .then((res) => res.json(res))
@@ -18,15 +20,16 @@ const loadData = (id) => {
   fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     .then((res) => res.json())
     .then((data) => setCard(data.data));
-  };
-  
-  const setCard = (data) => {
-    console.log(data);
-    if (data.length == 0) {
-      const cardContainer = document.getElementById("card-container");
-      cardContainer.classList.remove("grid");
-      cardContainer.innerHTML = "";
-      cardContainer.innerHTML = `
+};
+
+const setCard = (data) => {
+  console.log(data);
+
+  if (data.length == 0) {
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.classList.remove("grid");
+    cardContainer.innerHTML = "";
+    cardContainer.innerHTML = `
       <div class="h-[70vh] text-center flex flex-col items-center justify-center">
       <img src="./Icon.png" />
       <h1 class="text-[32px]">Oops!! Sorry, There is no content here</h1>  
@@ -43,8 +46,8 @@ const loadData = (id) => {
       const time = (min / 60).toFixed(2);
       const card = document.createElement("div");
       card.innerHTML = `
-    <div class="card w-96 bg-base-100 shadow-xl">
-    <figure class="relative"><img class="w-[100%] h-[216px]" src=${
+    <div class="card w-96 bg-base-100">
+    <figure class="relative"><img class="rounded-2xl w-[100%] h-[216px]" src=${
       singleData.thumbnail
     } alt="Shoes" />
     <p class="px-[6px] bg-[#171717] text-[#fff] bottom-[12px] right-[12px] rounded absolute">${
@@ -57,23 +60,38 @@ const loadData = (id) => {
       singleData.authors[0].profile_picture
     }/>
     <h2 class="card-title">
-    ${singleData.title}<span>${
+    ${singleData.title}
+    </h2>
+    </div> 
+    <div class="flex gap-2">
+    <h2 class="text-[#5d5d5d] text-[14px]">${
+      singleData.authors[0].profile_name
+    }</h2><span>${
         singleData.authors[0].verified
           ? '<img src="./fi_10629607.svg">'
           : "<img/>"
       } </span>
-    </h2>
-    </div> 
-    <p class="text-[#5d5d5d] text-[14px]">${
-      singleData.authors[0].profile_name
-    }</p>
+    </div>
       <p class="text-[#5d5d5d] text-[14px]">${singleData.others.views} views</p>
     </div>
     </div>
     `;
       cardContainer.appendChild(card);
     });
+    array = data;
   }
 };
 
 loadData(1000);
+
+document.getElementById("btn-blog").addEventListener("click", function () {
+  window.location = "./blog.html";
+});
+const sortBtn = document
+  .getElementById("sort-btn")
+  .addEventListener("click", function () {
+    const sorted = array.sort(
+      (a, b) => parseInt(a.others.views) - parseInt(b.others.views)
+    );
+    setCard(sorted);
+  });
